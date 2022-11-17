70c947b9b289c87193aef599ca78bc018cfe9490
Dhirendra Kumawat <31619054+dhirenkumavat@users.noreply.github.com>
2022-11-17 17:46:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,456 @@
+<!DOCTYPE html>
+
+<html class="no-js" lang="zxx">
+  <head>
+    <!-- metas -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Dhirendra portfolio" />
+    <meta
+      name="keywords"
+      content="Dhirendra portfolio,php developer, ci , laravel, React, Nodejs "
+    />
+
+    <!-- title -->
+    <title>Dhirendra</title>
+
+    <!-- favicon -->
+    <link href="img/favicon.ico" rel="icon" type="image/png" />
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+    <!-- google fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800"
+      rel="stylesheet"
+    />
+
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+
+    <!-- magnific-popup CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+
+    <!-- animate.min CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" />
+
+    <!-- Font Icon Core CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/et-line.css" />
+
+    <!-- Core Style Css -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!--[if lt IE 9]-->
+    <script src="js/html5shiv.min.js"></script>
+    <!--[endif]-->
+  </head>
+
+  <body>
+    <!-- ====== Preloader ======  -->
+    <div class="loading">
+      <div class="load-circle"></div>
+    </div>
+    <!-- ======End Preloader ======  -->
+
+    <!-- ====== Navgition ======  -->
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button
+            type="button"
+            class="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#nav-icon-collapse"
+            aria-expanded="false"
+          >
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <!-- logo -->
+          <a class="logo" href="#">Dhirendra Kumavat</a>
+        </div>
+
+        <!-- Collect the nav links, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="nav-icon-collapse">
+          <!-- links -->
+          <ul class="nav navbar-nav navbar-right">
+            <!-- <li><a href="#" data-scroll-nav="0" class="active">Home</a></li>
+            <li><a href="#" data-scroll-nav="1">About</a></li>
+            <li><a href="#" data-scroll-nav="2">Services</a></li>
+            <li><a href="#" data-scroll-nav="3">Works</a></li> -->
+            <!-- <li><a href="#" data-scroll-nav="4">Clients</a></li> -->
+            <!-- <li><a href="#" data-scroll-nav="5">Blog</a></li>
+<li><a href="#" data-scroll-nav="6">Contact</a></li> -->
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container -->
+    </nav>
+    <!-- ====== End Navgition ======  -->
+
+    <!-- ====== Header ======  -->
+    <section
+      id="home"
+      class="header"
+      data-scroll-index="0"
+      style="background-image: url(img/cheakamus-lake-whistler-og1.jpg)"
+      data-stellar-background-ratio="0.8"
+    >
+      <div class="v-middle">
+        <div class="container">
+          <div class="row">
+            <!-- caption -->
+            <div class="caption">
+              <h5>Hello</h5>
+              <h1 class="cd-headline clip">
+                <span class="blc">I Am</span>
+                <span class="cd-words-wrapper">
+                  <b class="is-visible">Dhirendra Kumavat</b>
+                  <b>Web Devloper</b>
+                  <b>PHP Devloper</b>
+                  <b>Codeigniter Devloper</b>
+                  <b>Laravel Devloper</b>
+                  <b>Nodejs Devloper</b>
+                  <b>Reactjs Devloper</b>
+                </span>
+              </h1>
+
+              <!-- social icons -->
+              <div class="social-icon">
+                <a href="https://github.com/dhirenkumavat" target="_blank">
+                  <span><i class="fa fa-github" aria-hidden="true"></i></span>
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/dhiren-kumavat-206b3064/"
+                  target="_blank"
+                >
+                  <span><i class="fa fa-linkedin" aria-hidden="true"></i></span>
+                </a>
+                <a href="skype:live:dhirenkumavat07?chat" target="_blank">
+                  <span><i class="fa fa-skype" aria-hidden="true"></i></span>
+                </a>
+                <a href="mailto: dhirenkumavat07@gmail.com">
+                  <span><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                </a>
+              </div>
+            </div>
+            <!-- end caption -->
+          </div>
+        </div>
+        <!-- /row -->
+      </div>
+      <!-- /container -->
+    </section>
+    <!-- ====== End Header ======  -->
+
+    <!-- ====== Hero ======  -->
+    <section class="hero section-padding pb-70" data-scroll-index="1">
+      <div class="container">
+        <div class="row">
+          <!-- hero image -->
+
+          <!-- content -->
+          <div class="col-md-12">
+            <div class="content mb-30">
+              <h3>About Me.</h3>
+              <span class="sub-title">Web Developer</span>
+              <p style="font-family: Raleway;line-height: 32px;font-weight: 500;font-style: normal;color: #323232;font-size: 24px;">
+                I am
+                <b>Dhirendra Kumavat</b>
+I have 6+ years of Experience as Full Stack  Developer with a demonstrated history of working in the information technology and services industry. Skilled in:NodeJs,
+React Js,Laravel, Codeigniter, PHP, Web Services, and Web Development.
+If you're looking to hire a professional Web programmer or freelance Web developer for your web project.
+              </p>
+
+              <!-- skills progress -->
+              <div class="skills">
+
+
+            </div>
+          </div>
+        </div>
+        <!-- /row -->
+      </div>
+      <!-- /container -->
+    </section>
+    <!-- ====== End Hero ======  -->
+
+    <!-- ====== Services ======  -->
+   <section
+      class="services section-padding bg-gray text-center pb-70"
+      data-scroll-index="2"
+    >
+      <div class="container">
+
+       <div class="section-head">
+          <h3>My Approach</h3>
+        </div>
+<span class="sub-title" style="
+    margin: auto;
+    font-family: Raleway;
+    line-height: 32px;
+    font-weight: 500;
+    font-style: normal;
+    color: #323232;
+    font-size: 20px;
+">What makes a great website? Here's my opinion: a successful website should be attractive but easy to read. A fascinating message, together with an immediate understanding. Original content and easily traceable information: that's my recipe! A high attention to detail every step of the way.
+</span>
+        <div class="row">
+
+          <div class="col-md-2">
+            <div class="item">
+              <span class="icon">
+                <i class="fa fa-lightbulb-o" aria-hidden="true"></i>
+              </span>
+              <h6>Discover</h6>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="item">
+              <span class="icon">
+                <i class="fa fa-list" aria-hidden="true"></i>
+              </span>
+              <h6>Plan</h6>
+            </div>
+          </div>
+         <div class="col-md-2">
+            <div class="item">
+              <span class="icon">
+                <i class="fa fa-paint-brush" aria-hidden="true"></i>
+              </span>
+              <h6>Design</h6>
+            </div>
+          </div>
+  <div class="col-md-2">
+            <div class="item">
+              <span class="icon">
+                <i class="fa fa-puzzle-piece" aria-hidden="true"></i>
+              </span>
+              <h6>Build</h6>
+            </div>
+          </div>
+    <div class="col-md-2">
+            <div class="item">
+              <span class="icon">
+                <i class="fa fa-plane" aria-hidden="true"></i>
+              </span>
+              <h6>Launch</h6>
+            </div>
+          </div>
+<div class="col-md-2">
+            <div class="item">
+              <span class="icon">
+                <i class="fa fa-check-circle" aria-hidden="true"></i>
+              </span>
+              <h6>Improve</h6>
+            </div>
+          </div>
+        </div>
+
+      </div>
+					</container>
+    </section>
+    <!--  ====== End Services ======  -->
+
+
+    <!--====== Numbers ======-->
+    <div class="numbers section-padding text-center pb-70" style="background-color: rgba(68, 19, 105, 0.8);">
+      <div class="container">
+        <div class="row">
+          <!-- items -->
+          <div class="col-md-3">
+            <div class="item">
+              <span class="icon" style="color: white;">
+                <i class="fa fa-users" aria-hidden="true"></i>
+              </span>
+              <h3 class="counter" style="color: white;">05</h3>
+              <p style="font-family: Raleway;line-height: 32px;font-weight: 500;font-style: normal;color: white;font-size: 24px;">Happy Customers</p>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="item">
+              <span class="icon" style="color: white;">
+                <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+              </span>
+              <h3 class="counter" style="color: white;">15</h3>
+              <p style="font-family: Raleway;line-height: 32px;font-weight: 500;font-style: normal;color:white;font-size: 24px;">Complete Projects</p>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="item">
+              <span class="icon" style="color: white;">
+                <i class="fa fa-bullhorn" aria-hidden="true"></i>
+              </span>
+              <h3 class="counter" style="color: white;">100000</h3>
+              <p style="font-family: Raleway;line-height: 32px;font-weight: 500;font-style: normal;color: white;font-size: 24px;">Lines Of Code</p>
+            </div>
+          </div>
+
+          <div class="col-md-3">
+            <div class="item">
+              <span class="icon" style="color: white;">
+                <i class="fa fa-cloud-download" aria-hidden="true"></i>
+              </span>
+              <h3 class="counter" style="color: white;">250</h3>
+              <p style="font-family: Raleway;line-height: 32px;font-weight: 500;font-style: normal;color: white;font-size: 24px;">Files Downloaded</p>
+            </div>
+          </div>
+        </div>
+        <!-- /row -->
+      </div>
+      <!-- /container -->
+    </div>
+    <!--====== End Numbers ======-->
+ <section class="services section-padding bg-gray text-center"data-scroll-index="2">
+      <div class="container">
+
+       <div class="section-head">
+          <h3>GET IN TOUCH</h3>
+        </div>
+        <div class="row">
+
+          <div class="col-md-12 ht-50">
+            <div class="">
+            <p style="width: 65%;margin: auto;font-family: Raleway;line-height: 32px;font-weight: 500;font-style: normal;color: #323232;font-size: 24px;">
+                We always look up to doing our best work and working with the best people. Need to start your next big thing or revive your brand with a new approach?
+
+
+              </p>
+
+            </div>
+          </div>
+         </div>
+      </div>
+					</container>
+    </section>
+     <!--====== End Services ======  -->
+<section class="services  bg-gray text-center"data-scroll-index="2">
+  <div class="container">
+
+	<div class="row">
+	  <div class="col-md-7">
+
+
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235526.90924555395!2d75.72376116522601!3d22.72422843081617!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fcad1b410ddb%3A0x96ec4da356240f4!2sIndore%2C%20Madhya%20Pradesh!5e0!3m2!1sen!2sin!4v1668686699840!5m2!1sen!2sin" width="100%" height="315" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <div class="col-md-5">
+       <form name='form1' method='post' >
+
+          <div class="form-group">
+            <input type="text" class="form-control" name="first_name" value="" placeholder="Name">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" name="email" value="" placeholder="E-mail">
+          </div>
+          <div class="form-group">
+            <input type="tel" class="form-control" name="phone" value="" placeholder="Phone">
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" name="message" rows="3" placeholder="Message"></textarea>
+          </div>
+          <input class="btn btn-default" type="submit" name="Submit" value="Submit">
+
+          </input>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+    <!--====== Blog ======-->
+
+    <!--====== End Blog ======-->
+
+    <!--====== Footer ======-->
+    <footer>
+      <div class="container text-center">
+        <div class="row">
+          <p>Copy Right 2022-2026 &copy; By Dhirendra Kumavat All Rights Reserved</p>
+        </div>
+      </div>
+    </footer>
+    <!--====== End Footer ======-->
+    <link
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+      rel="stylesheet"
+    />
+
+    <!-- jQuery -->
+    <link
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+      rel="stylesheet"
+    />
+
+    <script src="js/jquery-3.0.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.0/jquery-migrate.min.js"></script>
+
+    <!-- bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- scrollIt -->
+    <script src="js/scrollIt.min.js"></script>
+
+    <!-- magnific-popup -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+
+    <!-- owl carousel -->
+    <script src="js/owl.carousel.min.js"></script>
+
+    <!-- stellar js -->
+    <script src="js/jquery.stellar.min.js"></script>
+
+    <!-- animated.headline -->
+    <script src="js/animated.headline.js"></script>
+
+    <!-- jquery.waypoints.min js -->
+    <script src="js/jquery.waypoints.min.js"></script>
+
+    <!-- jquery.counterup.min js -->
+    <script src="js/jquery.counterup.min.js"></script>
+
+    <!-- isotope.pkgd.min js -->
+    <script src="js/isotope.pkgd.min.js"></script>
+
+    <!-- validator js -->
+    <script src="js/validator.js"></script>
+
+    <!-- custom script -->
+    <script src="js/custom.js"></script>
+<?php
+if(isset($_POST['submit'])){
+    $to = "website@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+				 $phone = $_POST['phone'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . "  wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . " . $phone . "."\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+  </body>
+</html>
